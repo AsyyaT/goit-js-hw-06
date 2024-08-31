5ace9590420091fe1784c1663916e241e5b7aebf
anastasiia <aizatsana15@gmail.com>
2024-08-31 19:34:37 +0300

    HW-6 added necessary classes

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items
+  }
+  getItems() {
+    return this.#items
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem)
+  }
+
+  removeItem(itemToRemove) {
+    const objToRemove = this.#items.indexOf(itemToRemove)
+    if (objToRemove !== -1) {
+      this.#items.splice(1, objToRemove)
+    }
+  }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
